7ca1df4273df07d888717fdf7e346ec0106dd049
FIGBERT <figbert@figbert.com>
2021-06-24 13:29:37 -0700

    Extend arrays and dictionaries for UserDefaults
    The new extensions for arrays and dictionaries add RawRepresentable
    conformance by converting back and forth between JSON on demand to allow
    for storage as Strings in UserDefaults using the @AppStorage property
    wrapper.

---
+++ Privacy Redirect for Safari/RawRepresentable/Dictionary+RawRepresentable.swift
@@ -0,0 +1,28 @@
+//
+//  Dictionary+RawRepresentable.swift
+//  Privacy Redirect for Safari
+//
+//  Created by FIGBERT on 6/24/21.
+//
+
+import Foundation
+
+extension Dictionary: RawRepresentable where Key: Codable, Value: Codable {
+    public init?(rawValue: String) {
+        guard let data = rawValue.data(using: .utf8),
+              let result = try? JSONDecoder().decode([Key: Value].self, from: data)
+        else {
+            return nil
+        }
+        self = result
+    }
+
+    public var rawValue: String {
+        guard let data = try? JSONEncoder().encode(self),
+              let result = String(data: data, encoding: .utf8)
+        else {
+            return "{}"
+        }
+        return result
+    }
+}
