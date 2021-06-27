d74f3d09b3e8de366189aac897c01b1c3dcb00fa
FIGBERT <figbert@figbert.com>
2021-06-26 23:26:02 -0700

    Add settings retrieval to background.js

--- Privacy Redirect for Safari Extension/pages/background.js
+++ Privacy Redirect for Safari Extension/pages/background.js
@@ -3,5 +3,15 @@
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type == "Open Sesame") {
     browser.runtime.sendNativeMessage({ message: "Open Sesame" });
+  } else if (request.type == "redirectSettings") {
+    browser.runtime.sendNativeMessage("application.id", { message: "redirectSettings" }, function(response) {
+      sendResponse(response);
+    });
+    return true;
+  } else if (request.type == "instanceSettings") {
+    browser.runtime.sendNativeMessage("application.id", { message: "instanceSettings" }, function(response) {
+      sendResponse(response);
+    });
+    return true;
   }
 })
