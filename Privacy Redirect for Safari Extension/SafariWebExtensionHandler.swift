ba179f4a48d4bddc11dd7d2242a1c744c294bf80
FIGBERT <figbert@figbert.com>
2021-06-26 23:24:17 -0700

    Split settings response in app extension

--- Privacy Redirect for Safari Extension/SafariWebExtensionHandler.swift
+++ Privacy Redirect for Safari Extension/SafariWebExtensionHandler.swift
@@ -25,8 +25,28 @@ class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
         if messageDict?["message"] == "Open Sesame" && app != nil {
             NSWorkspace.shared.openApplication(at: app!, configuration: NSWorkspace.OpenConfiguration())
             response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
-        } else if messageDict?["message"] == "settings" {
-            response.userInfo = [ SFExtensionMessageKey: defaults?.dictionaryRepresentation() as Any ]
+        } else if messageDict?["message"] == "redirectSettings" {
+            response.userInfo = [
+                SFExtensionMessageKey: [
+                    "redirectNitter": defaults?.bool(forKey: "redirectNitter"),
+                    "redirectReddit": defaults?.bool(forKey: "redirectReddit"),
+                    "redirectInvidious": defaults?.bool(forKey: "redirectInvidious"),
+                    "redirectSimplyTranslate": defaults?.bool(forKey: "redirectSimplyTranslate"),
+                    "redirectOsm": defaults?.bool(forKey: "redirectOsm"),
+                    "redirectSearchEngine": defaults?.bool(forKey: "redirectSearchEngine"),
+                ]
+            ]
+        } else if messageDict?["message"] == "instanceSettings" {
+            response.userInfo = [
+                SFExtensionMessageKey: [
+                    "nitterInstance": defaults?.string(forKey: "nitterInstance"),
+                    "redditInstance": defaults?.string(forKey: "redditInstance"),
+                    "invidiousInstance": defaults?.string(forKey: "invidiousInstance"),
+                    "simplyTranslateInstance": defaults?.string(forKey: "simplyTranslateInstance"),
+                    "osmInstance": defaults?.string(forKey: "osmInstance"),
+                    "searchEngineInstance": defaults?.string(forKey: "searchEngineInstance"),
+                ]
+            ]
         }
 
         context.completeRequest(returningItems: [response], completionHandler: nil)
