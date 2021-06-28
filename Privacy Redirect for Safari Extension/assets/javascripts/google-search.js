a79442fb431a0247ffad34bb7559f157642d36d8
FIGBERT <figbert@figbert.com>
2021-06-28 11:48:20 -0700

    Update google-search.js to use native settings

--- Privacy Redirect for Safari Extension/assets/javascripts/google-search.js
+++ Privacy Redirect for Safari Extension/assets/javascripts/google-search.js
@@ -1,49 +1,6 @@
 "use strict";
 
-const searchEngineInstances = [
-  { link: "https://duckduckgo.com", q: "/" },
-  { link: "https://startpage.com", q: "/search/" },
-  { link: "https://www.ecosia.org", q: "/search" },
-  { link: "https://www.qwant.com", q: "/" },
-  { link: "https://www.mojeek.com", q: "/search" },
-  { link: "https://search.snopyta.org", q: "/" },
-  { link: "https://searx.info", q: "/" },
-  { link: "https://searx.be", q: "/" },
-  { link: "https://search.disroot.org", q: "/" },
-  { link: "https://searx.tuxcloud.net", q: "/" },
-  { link: "https://searx.ninja", q: "/" },
-  { link: "https://tromland.org/searx", q: "/search" },
-  { link: "https://engine.presearch.org", q: "/search" },
-  { link: "https://searx.silkky.cloud", q: "/" },
-  { link: "https://search.trom.tf", q: "/" },
-  { link: "https://whooglesearch.net", q: "/search" },
-  { link: "https://whoogle.sdf.org", q: "/search" },
-  { link: "https://whoogle.himiko.cloud", q: "/search" },
-  { link: "https://whoogle-search.zeet.app", q: "/search" },
-];
-let disableSearchEngine;
-let searchEngineInstance;
-let exceptions;
-
-window.browser = window.browser || window.chrome;
-
-function getRandomInstance() {
-  return searchEngineInstances[~~(searchEngineInstances.length * Math.random())];
-}
-
-function isNotException(url) {
-  return !exceptions.some((regex) => regex.test(url.href));
-}
-
-function shouldRedirect(url) {
-  return (
-    isNotException(url) &&
-    !disableSearchEngine &&
-    !url.pathname.includes("/sorry")
-  );
-}
-
-function redirectGoogleSearch(url) {
+function redirectGoogleSearch(instance, url) {
   let search = "";
   url.search
     .slice(1)
@@ -51,28 +8,24 @@ function redirectGoogleSearch(url) {
     .forEach(function (input) {
       if (input.startsWith("q=")) search = input;
     });
-  return `${searchEngineInstance.link}${searchEngineInstance.q}?${search}`;
+  return `https://${instance}?${search}`;
 }
 
-browser.storage.sync.get(
-  [
-    "disableSearchEngine",
-    "searchEngineInstance",
-    "exceptions",
-  ],
-  (result) => {
-    disableSearchEngine = result.disableSearchEngine;
-    searchEngineInstance = result.searchEngineInstance || getRandomInstance();
-    exceptions = result.exceptions
-      ? result.exceptions.map((e) => {
-          return new RegExp(e);
-        })
-      : [];
-    const url = new URL(window.location);
-    if (shouldRedirect(url)) {
-      const redirect = redirectGoogleSearch(url);
-      console.info("Redirecting", `"${url.href}"`, "=>", `"${redirect}"`);
-      window.location = redirect;
+browser.runtime.sendMessage({ type: "redirectSettings" })
+  .then(redirects => {
+    if (redirects.searchEngine) {
+      return browser.runtime.sendMessage({ type: "instanceSettings" });
+    } else {
+      return null;
     }
-  }
-);
+  })
+  .then(instances => {
+    if (instances) {
+      const url = new URL(window.location);
+      if (!url.pathname.includes("/sorry")) {
+        const redirect = redirectGoogleSearch(instances.searchEngine, url);
+        console.info(`Redirecting ${url.href} => ${redirect}`);
+        window.location = redirect;
+      }
+    }
+  });
