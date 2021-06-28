508d3c894432763117841b080b3810208440c289
FIGBERT <figbert@figbert.com>
2021-06-28 00:01:24 -0700

    Update google-translate.js to use native settings
    The update does not yet support exceptions, which will come in future
    commits.

--- Privacy Redirect for Safari Extension/assets/javascripts/google-translate.js
+++ Privacy Redirect for Safari Extension/assets/javascripts/google-translate.js
@@ -1,50 +1,18 @@
 "use strict";
 
-const simplyTranslateInstances = ["https://translate.metalune.xyz"];
-let disableSimplyTranslate;
-let simplyTranslateInstance;
-let exceptions;
-
-window.browser = window.browser || window.chrome;
-
-function getRandomInstance() {
-  return simplyTranslateInstances[~~(simplyTranslateInstances.length * Math.random())];
-}
-
-function isNotException(url) {
-  return !exceptions.some((regex) => regex.test(url.href));
-}
-
-function shouldRedirect(url) {
-  return (
-    isNotException(url) &&
-    !disableSimplyTranslate
-  );
-}
-
-function redirectGoogleTranslate(url) {
-  return `${simplyTranslateInstance}/${url.search}`;
-}
-
-browser.storage.sync.get(
-  [
-    "disableSimplyTranslate",
-    "simplyTranslateInstance",
-    "exceptions",
-  ],
-  (result) => {
-    disableSimplyTranslate = result.disableSimplyTranslate;
-    simplyTranslateInstance = result.simplyTranslateInstance || getRandomInstance();
-    exceptions = result.exceptions
-      ? result.exceptions.map((e) => {
-          return new RegExp(e);
-        })
-      : [];
-    const url = new URL(window.location);
-    if (shouldRedirect(url)) {
-      const redirect = redirectGoogleTranslate(url);
-      console.info("Redirecting", `"${url.href}"`, "=>", `"${redirect}"`);
+browser.runtime.sendMessage({ type: "redirectSettings" })
+  .then(redirects => {
+    if (redirects.simplyTranslate) {
+      return browser.runtime.sendMessage({ type: "instanceSettings" })
+    } else {
+      return null;
+    }
+  })
+  .then(instances => {
+    if (instances) {
+      const url = new URL(window.location);
+      const redirect = `https://${instances.simplyTranslate}/${url.search}`;
+      console.info(`Redirecting ${url.href} => ${redirect}`);
       window.location = redirect;
     }
-  }
-);
+  });
