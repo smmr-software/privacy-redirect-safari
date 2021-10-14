6f5d2f695a9ed5eeb8572ff67276c7cf07b87498
FIGBERT <figbert@figbert.com>
2021-10-14 12:01:34 -0700

    Add functionality to iOS extension
    This commit adds redirect functionality to the extension on iOS. To do
    this, the iOS, macOS, and Shared folders have been sorted properly. The
    iOS target has no popup, which the macOS version uses to allow users to
    open the app from the browser.

--- iOS (Extension)/Resources/background.js
+++ iOS (Extension)/Resources/background.js
@@ -1,6 +1,15 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+"use strict";
 
-    if (request.greeting === "hello")
-        sendResponse({ farewell: "goodbye" });
-});
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type == "redirectSettings") {
+    browser.runtime.sendNativeMessage("application.id", { message: "redirectSettings" }, function(response) {
+      sendResponse(response);
+    });
+    return true;
+  } else if (request.type == "instanceSettings") {
+    browser.runtime.sendNativeMessage("application.id", { message: "instanceSettings" }, function(response) {
+      sendResponse(response);
+    });
+    return true;
+  }
+})
