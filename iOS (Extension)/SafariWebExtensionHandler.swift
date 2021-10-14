19042d7ce7b6d6815720772871d4e4124b611371
FIGBERT <figbert@figbert.com>
2021-10-14 12:01:34 -0700

    Add functionality to iOS extension
    This commit adds redirect functionality to the extension on iOS. To do
    this, the iOS, macOS, and Shared folders have been sorted properly. The
    iOS target has no popup, which the macOS version uses to allow users to
    open the app from the browser.

--- iOS (Extension)/SafariWebExtensionHandler.swift
+++ iOS (Extension)/SafariWebExtensionHandler.swift
@@ -8,6 +8,8 @@
 import SafariServices
 import os.log
 
+let SFExtensionMessageKey = "message"
+
 class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
 
     func beginRequest(with context: NSExtensionContext) {
@@ -15,10 +17,41 @@ class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
         let message = item.userInfo?[SFExtensionMessageKey]
         os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
 
+        let defaults = UserDefaults(suiteName: "\(Bundle.main.object(forInfoDictionaryKey: "TeamIdentifierPrefix") ?? "9WHCFZ6J4N.")Privacy-Redirect-for-Safari")
         let response = NSExtensionItem()
-        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
+        
+        let messageDict = message as? [String: String]
+        if messageDict?["message"] == "redirectSettings" {
+            response.userInfo = [
+                SFExtensionMessageKey: [
+                    "nitter": !(defaults?.bool(forKey: "disableNitter") ?? false),
+                    "reddit": !(defaults?.bool(forKey: "disableReddit") ?? false),
+                    "invidious": !(defaults?.bool(forKey: "disableInvidious") ?? false),
+                    "simplyTranslate": !(defaults?.bool(forKey: "disableSimplyTranslate") ?? false),
+                    "osm": !(defaults?.bool(forKey: "disableOsm") ?? false),
+                    "searchEngine": !(defaults?.bool(forKey: "disableSearchEngine") ?? false),
+                ]
+            ]
+        } else if messageDict?["message"] == "instanceSettings" {
+            let nitter = defaults?.string(forKey: "nitterInstance") ?? "nitter.net"
+            let reddit = defaults?.string(forKey: "redditInstance") ?? "teddit.net"
+            let invidious = defaults?.string(forKey: "invidiousInstance") ?? "invidious.snopyta.org"
+            let simplyTranslate = defaults?.string(forKey: "simplyTranslateInstance") ?? "translate.metalune.xyz"
+            let osm = defaults?.string(forKey: "osmInstance") ?? "openstreetmap.org"
+            let searchEngine = defaults?.string(forKey: "searchEngineInstance") ?? "duckduckgo.com/"
+            response.userInfo = [
+                SFExtensionMessageKey: [
+                    "nitter": nitter,
+                    "reddit": reddit,
+                    "invidious": invidious,
+                    "simplyTranslate": simplyTranslate,
+                    "osm": osm,
+                    "searchEngine": searchEngine,
+                ]
+            ]
+        }
 
         context.completeRequest(returningItems: [response], completionHandler: nil)
     }
-
+    
 }
