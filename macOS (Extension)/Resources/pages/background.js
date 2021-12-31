05a4ef1eab01e66b33be23d7b47f4b468316a50a
FIGBERT <figbert@figbert.com>
2021-12-30 17:12:55 -0800

    Lint and format macOS background.js with deno

--- macOS (Extension)/Resources/pages/background.js
+++ macOS (Extension)/Resources/pages/background.js
@@ -1,17 +1,21 @@
 "use strict";
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+browser.runtime.onMessage.addListener((request, _, sendResponse) => {
   if (request.type == "Open Sesame") {
     browser.runtime.sendNativeMessage({ message: "Open Sesame" });
   } else if (request.type == "redirectSettings") {
-    browser.runtime.sendNativeMessage("application.id", { message: "redirectSettings" }, function(response) {
+    browser.runtime.sendNativeMessage("application.id", {
+      message: "redirectSettings",
+    }, function (response) {
       sendResponse(response);
     });
     return true;
   } else if (request.type == "instanceSettings") {
-    browser.runtime.sendNativeMessage("application.id", { message: "instanceSettings" }, function(response) {
+    browser.runtime.sendNativeMessage("application.id", {
+      message: "instanceSettings",
+    }, function (response) {
       sendResponse(response);
     });
     return true;
   }
-})
+});
