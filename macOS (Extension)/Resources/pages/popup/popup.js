4529b9259899e52e3bbdff2952590bac88f94a43
FIGBERT <figbert@figbert.com>
2021-12-30 17:13:35 -0800

    Format macOS extension popup.js with deno

--- macOS (Extension)/Resources/pages/popup/popup.js
+++ macOS (Extension)/Resources/pages/popup/popup.js
@@ -1,5 +1,5 @@
 "use strict";
 
 document.querySelector("#open").addEventListener("click", () => {
-  browser.runtime.sendMessage({type: "Open Sesame"});
+  browser.runtime.sendMessage({ type: "Open Sesame" });
 });
